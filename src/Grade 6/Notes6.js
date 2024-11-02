734b86e9427c0a4db714e0209341156e72425f5c
Pabasara123991 <bodahandihdesilva@gmail.com>
2024-11-02 21:52:57 +0530

    did some changes

--- src/Grade 6/Notes6.js
+++ src/Grade 6/Notes6.js
@@ -68,22 +68,22 @@ const Grade6Notes = () => {
         </header>
 
         {Object.entries(notesByInstitution).map(([institution, notes]) => (
-          <section key={institution} className="mb-12 mx-8">
+          <section key={institution} className="mb-12 mx-3">
             <h3 className="text-2xl font-semibold text-gray-700 mb-4">{institution}</h3>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               {notes.map((note) => (
-                <div key={note.id} className="bg-white rounded-lg shadow-md overflow-hidden transition-all duration-300 hover:shadow-lg">
+                <div key={note.id} className="bg-white rounded-lg shadow-md  transition-all duration-300 hover:shadow-lg">
                   <div className="p-6">
                     <h3 className="text-2xl font-semibold text-gray-800 mb-1">{note.title}</h3>
                     <p className="text-sm text-gray-500 mb-8">
                       {note.createdAt ? new Date(note.createdAt).toLocaleDateString() : 'Date Not Available'}
                     </p>
-                    <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center gap-x-3">
                       <a
                         href={note.fileURL}
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="flex items-center bg-primary px-3 py-2 rounded-md text-white "
+                        className="flex items-center bg-primary px-2 py-2 rounded-md text-white "
                       >
                         <Eye className="w-5 h-5 mr-2" />
                         View
@@ -91,7 +91,7 @@ const Grade6Notes = () => {
                       <a
                         href={note.fileURL}
                         download
-                        className="flex items-center bg-primary px-3 py-2 rounded-md text-white "
+                        className="flex items-center bg-primary px-2 py-2 rounded-md text-white "
                       >
                         <Download className="w-5 h-5 mr-2" />
                         Download
